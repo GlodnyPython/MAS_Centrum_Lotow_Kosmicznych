d6b455ade26ba011e7bba1280a5f4eac657939bf
MateuszJacoszek <matijot0@gmail.com>
2020-06-18 02:51:59 +0200

    [VIP] Person and MissionSpecialist

--- src/SpaceMissionControl/MissionSpecialist.java
+++ src/SpaceMissionControl/MissionSpecialist.java
@@ -1,4 +1,51 @@
 package SpaceMissionControl;
 
+import java.util.ArrayList;
+
 public class MissionSpecialist {
+
+    private Person person;
+
+    ArrayList<String> specialization = new ArrayList<>();
+
+
+    private MissionSpecialist(Person person){
+        this.person = person;
+    }
+    protected MissionSpecialist(Person person, String spec){
+        this.person = person;
+
+        addSpecialization(spec);
+    }
+
+    public void addSpecialization(String spec){
+
+        boolean append = false;
+
+        for ( String s : this.specialization){
+            if (s == spec){
+                append = true;
+                throw new NullPointerException("Specialization " + spec + " is already assigned");
+            }
+        }
+        if (append == false){
+            this.specialization.add(spec);
+        }
+    }
+
+    public void removeSpecialization(String spec){
+
+        boolean append = false;
+
+        for ( String s : this.specialization){
+            if (s == spec){
+                append = true;
+                this.specialization.remove(s);
+            }
+        }
+        if (append == false){
+            throw new NullPointerException("Specialization " + spec + " was not assigned to " + person.name + " " + person.surname);
+        }
+    }
+
 }
--- src/SpaceMissionControl/Person.java
+++ src/SpaceMissionControl/Person.java
@@ -1,4 +1,59 @@
 package SpaceMissionControl;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 public class Person {
+
+    private static ArrayList<Person> personsList = new ArrayList<>();
+    String name;
+    String surname;
+    Timestamp dateOfBirth;
+    int mass;
+
+    //TODO Commander
+    //TODO Pilot
+    //TODO Mission Specialist
+    MissionSpecialist ms;
+
+    protected Person (String name, String surname, Timestamp dateOfBirth, int mass) {
+        this.name        = name;
+        this.surname     = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.mass        = mass;
+
+    }
+
+    public static void addPerson (String name, String surname, Timestamp dateOfBirth, int mass) {
+        personsList.add(new Person(name, surname, dateOfBirth, mass));
+    }
+
+    public void updateMass(int mass){
+        this.mass = mass;
+    }
+
+    //TODO implementacja dziedziczenia
+
+    public void addSpecialization (String spec){
+        if (ms != null){
+            ms = new MissionSpecialist(this, spec);
+            //TODO @ here
+        }
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public String getInfo() {
+        String text = "Astronauta: \n" +
+                   "Name: "          + name +
+                "\n Surname: "       + surname +
+                "\n Date of Birth: " + dateOfBirth +
+                "\n Mass: "          + mass
+                ;
+
+        return text;
+    }
+
 }
